25daf8a7165e614e1cd382c17ad69aef00f3782c
Rodolphe <rodolphebansard@yahoo.com>
2021-05-20 21:44:12 +0200

    Dialog asset

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -8,18 +8,72 @@ public class Dialog : MonoBehaviour
 {
     public string fileName;
     public TMP_Text dialogText;
+    public float writeSpeed;
 
     private string[] dialogContent;
-    private int nbLines;
+
+    private IEnumerator writeText;
+    private int lineIndex;
+
+    private bool dialogIsRunning;
+    
 
     private void Start()
     {
+        dialogIsRunning = false;
+        lineIndex = 0;
         dialogText.text = "";
-        string[] content = File.ReadAllLines(Application.dataPath + "/TextFiles/" + fileName + ".txt");
-        nbLines = content.Length;
+        dialogContent = File.ReadAllLines(Application.dataPath + "/TextFiles/" + fileName + ".txt");
+        NextLine();
+    }
 
-        StartCoroutine
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if (dialogIsRunning)
+            {
+                dialogText.text = dialogContent[lineIndex];
+                StopCoroutine(writeText);
+                lineIndex++;
+                dialogIsRunning = false;
+            }
+            else
+            {
+                if(lineIndex < dialogContent.Length)
+                {
+                    NextLine();
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+
+
+
+    IEnumerator WriteText(string line)
+    {
+        dialogIsRunning = true;
+        dialogText.text = "";
+        for(int i = 0; i<line.Length; i++)
+        {
+            dialogText.text += line[i];
+            yield return new WaitForSeconds(writeSpeed);
+        }
+        lineIndex++;
+        dialogIsRunning = false;
+
+        
         
     }
 
+
+    private void NextLine()
+    {
+        writeText = WriteText(dialogContent[lineIndex]);
+        StartCoroutine(writeText);
+    }
 }
